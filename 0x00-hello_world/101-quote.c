9a1c32e2c1637fea9859f86c02166720c837fcb6
seundp <seundp@yahoo.com>
2023-02-17 05:59:05 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program to print a line to the standard error
+ * main - c program to print a line to the standard error
  * Return 1 (success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
