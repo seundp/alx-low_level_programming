4ab948eaea1636594c4d1f8453191dbc411d38e9
seundp <seundp@yahoo.com>
2023-02-21 19:59:33 -0800

    lastdigit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_last_digit-prints the last digit of an interger.
- * @a:number to compute last digit.
- * Return:last digit.
+ * print_last_digit-prints the last digit of an interger
+ * @a:number to compute last digit
+ * Return: last digit
  */
 
 int print_last_digit(int a)
