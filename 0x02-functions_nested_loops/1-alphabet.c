7a86183e114821a30dd1d1ceffaa46e26980beac
seundp <seundp@yahoo.com>
2023-02-21 17:37:43 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 
 void print_alphabet(void)
 {
-	char letters;
+	char letter;
 
 	for (letter = 'a'; letter <= 'z' ; letter++)
-		-putchar(letter);
+		_putchar(letter);
 
-	putchar('\n');
+	_putchar('\n');
 }
